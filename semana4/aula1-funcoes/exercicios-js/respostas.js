282bf33d0e07871162aa9c7935c925a074ee1b4d
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-11-18 22:44:05 -0300

    ainda incompleto, mas tem algumas respostas

---
+++ semana4/aula1-funcoes/exercicios-js/respostas.js
@@ -0,0 +1,19 @@
+//Respostas
+//1. dá a quantia em Reais convertida do dólar, com funções de multiplicação, e perguntando o câmbio atual ao usuário. no exemplo há 100 dólares, se o código rodasse, inserindo o valor 4.22 a mensagem no console seria R$422.
+//2.
+//3. 
+//a. false
+//b. false
+//c. true
+//d. true
+//e. false
+//4. 
+//5.
+//
+//
+//
+//
+//
+//
+//
+console.log("teste")

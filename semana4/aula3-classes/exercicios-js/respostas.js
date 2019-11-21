05d6839b3c0ffdd115835c9ee92af80243ef03f6
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-11-21 18:31:24 -0300

    duas primeiras questões respondidas

---
+++ semana4/aula3-classes/exercicios-js/respostas.js
@@ -0,0 +1,8 @@
+// Exercícios semana 4 aula 3
+// 1 - Cria um array com os números, e mais dois arrays vazios. Depois verifica quais números são divisíveis por 2 (pares) e separa nos outros arrays vazios. Os pares vão para o array 1 e os ímpares pro array 2.
+//     No final, as mensagens dizem a quantidade total de números dentro dos três arrays.
+// 2 - Cria uma array com os números, e mais duas variáveis, com valor infinito e outra com valor 0. Depois, verifica se o numero do array numeros é menor que infinito. (não consegui saber ao certo o que aconteceria)
+//     O segundo verifica se o número do array numeros é menor que 0, se sim o novo valor da variável numero2 sera igual ao número. então o console.log(numero2) deve dar a mensagem -10
+// 3 - Não, por que a variável quantidadeDeNumerosPares foi declarada com const, que não permite que ela seja alterada. substituíndo o const por let resolveria o problema.
+// 4 - 
+// 5 - 

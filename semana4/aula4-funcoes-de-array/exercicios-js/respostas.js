68b5183def7d7d6962e160bbe49d124507cef071
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-11-22 16:41:25 -0300

    primeiro exercicio de JS respondido

---
+++ semana4/aula4-funcoes-de-array/exercicios-js/respostas.js
@@ -0,0 +1,21 @@
+// Exercícios semana 4 aula 4
+// 1 - const professores = [
+//   { nome: "Darvas", idade: 20 },
+// 	 { nome: "Goli", idade: 10 },
+//	 { nome: "Paulinha", idade: 12 },
+//	 { nome: "Artur", idade: 89 },
+//	 { nome: "Soter", idade: 55 },
+//	 { nome: "Miau", idade: 1 },
+//	 { nome: "Lu", idade: 32 }
+// ]
+//    const professoresMaiorDeIdade = professores.filter((professor, index, array) => {
+//    if(professor.idade > 18){
+//    return true
+//  }
+//    return false
+// }) 
+// 2 - const array = [1, 2, 3, 4, 5, 6]
+//     
+// 3 - 
+// 4 - 
+// 5 - 

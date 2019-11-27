518a57baeac5efc1e25d42df61a6b66d4776b927
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-11-27 15:42:02 -0300

    primeiro commit. Estrutura feita

---
+++ semana5/aula3-compsclasse/instaex/instaex/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Instaex</h1>
+      </header>
+      <section className="App-main">
+        <div className="App-posts">
+          <h3>Foto</h3>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;

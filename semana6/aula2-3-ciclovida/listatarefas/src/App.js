9bc9672f4cb3de6f30a35ae4e75c796443ca8430
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-12-04 17:02:11 -0300

    formando estrutura

---
+++ semana6/aula2-3-ciclovida/listatarefas/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import PropTypes from 'prop-types';
+
+const Principal = styled.div `
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`
+
+const Tilulo = styled.h1 `
+text-align: center;
+`
+
+const InputTarefa = styled.input `
+margin-bottom: 10px;
+`
+
+const Adicionar = styled.button `
+
+`
+
+function App() {
+  return (
+    <Principal>
+      <Tilulo>Lista de tarefas</Tilulo>
+      <InputTarefa />
+      <Adicionar>
+        Adicionar
+      </Adicionar>
+
+    </Principal>
+  )
+}
+
+
+export default App;

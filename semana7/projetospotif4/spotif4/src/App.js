f687279ac6a32988f50e616a75bc2cbc8b4ed9d4
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-12-13 18:18:58 -0300

    trabalhando no post de playlists

---
+++ semana7/projetospotif4/spotif4/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SpotAPI from './components/SpotAPI'
+
+
+function App() {
+  return (
+
+    <div>
+      <header>
+        <h1>Spoti4</h1>
+      </header>
+      <section>
+        <p>Crie sua playlist</p>
+      </section>
+        
+    </div>
+    
+  )
+    
+  
+}
+
+export default App;

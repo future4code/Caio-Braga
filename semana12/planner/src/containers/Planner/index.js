e691a14a7942ce4312fa8e86fa502de6b72e8857
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-01-30 18:12:41 -0300

    fiz a parte estática do criar tarefas, mas ainda tenho problemas com as actions e o reducer

--- semana12/planner/src/containers/Planner/index.js
+++ semana12/planner/src/containers/Planner/index.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 import { getTasks, createTask } from "../../actions";
 import Input from "@material-ui/core/Input";
-import Button from "@material-ui/core/Button"
 
 const MainContainer = styled.div `
 display: flex;
@@ -12,15 +11,50 @@ justify-content: center;
 padding: 30px;
 margin: 0 auto;
 `
+const TaskButton = styled.button `
+width: 100px;
+`
+
+const Dropdown = styled.select `
+width: 150px;
+`
 
 
 class Planner extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      select: {}
+    }
+  }
+
+handleCreateTask = (event) => {
+  event.preventDefault();
+
+  const { text } = this.state.select
+
+  this.props.createTask(text)
+  this.setState({select:{}})
+}
+
   render() {
     return (
       <MainContainer>
         O Planner vai aqui
         <Input></Input>
-        <Button></Button>
+
+        <Dropdown>
+          <option>Segunda</option>
+          <option>Terça</option>
+          <option>Quarta</option>
+          <option>Quinta</option>
+          <option>Sexta</option>
+          <option>Sábado</option>
+          <option>Domingo</option>
+        </Dropdown>
+
+        <TaskButton onClick = {this.handleCreateTask}>Criar Tarefa</TaskButton>
+
       </MainContainer>
     );
   }

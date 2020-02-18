3aecb5ed2401282890904bc7ddd48e3d72468a06
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-02-18 18:50:11 -0300

    travei no exercício 5, na parte de trazer os valores dos pratos para a função do cashier que calcuta a conta

--- semana15/aula2/src/classes/saltydish.ts
+++ semana15/aula2/src/classes/saltydish.ts
@@ -16,7 +16,7 @@ export class SaltyDish extends Dish {
 }
 
 const feijoada = new SaltyDish(100, 20, ["leite condensado"], 100);
-const hamburger = new SaltyDish(80, 15, ["pão, carne, queijo, cebola, tomate"], 40);
+export const hamburger = new SaltyDish(80, 15, ["pão, carne, queijo, cebola, tomate"], 40);
 console.log(feijoada.getProfit());
 console.log(hamburger.getProfit());
 

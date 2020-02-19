e9729e30176dee1c34107ea6896a62880420b645
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-02-19 17:16:46 -0300

    corrigi o index, que estava como .js

--- semana15/aula3/src/index.js
+++
@@ -1,4 +0,0 @@
-import { ResidentialClient } from './residentialclient';
-
-const almeida = new ResidentialClient("Almeida", 5, 10, "555.555.555-55", 50367-475)
-const pacheco = new ResidentialClient("Pacheco", 9, 15, "7777.7777.777-77", 20385-349)

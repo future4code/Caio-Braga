c27666e0aa962be42f69e501233771fd9df29606
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-02-20 16:46:16 -0300

    classe HandleError renomeada para ErrorPrinter

--- semana15/aula4/src/error.ts
+++ semana15/aula4/src/error.ts
@@ -1,8 +1,13 @@
 import * as moment from 'moment';
 moment.locale("pt-br")
 
-class HandleError {
+export class ErrorPrinter {
     public message: string
     public timestamp: Date 
     Date = moment("DD/MM/YYYY HH:mm:ss")
+
+    getError(message: string, timestamp: Date) {
+        return console.log(message, "-", timestamp )
+    }
 }
+

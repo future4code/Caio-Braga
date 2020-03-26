702b920f20b643546f9c73723e4c78ee0c4017f3
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-03-26 18:33:01 -0300

    estrutura básica de signup e login criada, faltando bando de dados

--- semana19/futurebook/src/presentation/index.ts
+++ semana19/futurebook/src/presentation/index.ts
@@ -1,33 +1,34 @@
 import express, { Request, Response } from "express";
+import { signupEndpoint } from "./endpoints/signup";
+import { loginEndpoint } from "./endpoints/login";
+import { SignupUC } from "../business/usecase/signupUC";
 
 
-// const app = express();
-// app.use(express.json());
+const app = express();
+app.use(express.json());
 
-// app.post('/signup', signupEndpoint, async (request: Request, response: Response) => {
-//     const useCase = new SignupUC()
+app.post('/signup', signupEndpoint, async (request: Request, response: Response) => {
+    const useCase = new SignupUC()
 
-//     const input = {
-//         email: request.body.email,
-//         password: request.body.password,
-//         name: request.body.name,
-//         dateOfBirth: request.body.dateOfBirth
-//     }
+    const input = {
+        email: request.body.email,
+        password: request.body.password,
+        name: request.body.name,
+    }
 
-//     try {
-//         await useCase.execute(input)
+    try {
+        await useCase.execute(input)
 
-//     response.send({message: "Usuário criado"})
-//     } catch(e) {
-//         response.status(500).send({message: e.message})
-//     }
+    response.send({message: "Usuário criado"})
+    } catch(e) {
+        response.status(500).send({message: e.message})
+    }
     
-// })
+})
 
-// app.post('/login', loginEndpoint);
-// app.get('/getUser', getUserInfoEndpoint)
-// app.post('/create/recipe', getRecipesEndPoint)
-// app.post('/user/follow', followUserEndpoint)
-// app.get('/feed', getFeedEndPoint)
+app.post('/login', loginEndpoint);
+app.get('/getUser', getUserInfoEndpoint)
+app.post('/user/follow', followUserEndpoint)
+app.get('/feed', getFeedEndPoint)
 
 export default app;

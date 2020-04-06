4fa311c1ddfc0125506f91660fe55d34c59c5d57
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-04-06 01:17:39 -0300

    criados os arquivos dos endpoints

---
+++ semana20/f4ma/src/presentation/endpoints/registerBand.ts
@@ -0,0 +1,6 @@
+import {Request, Response} from 'express';
+import {BandDatabase} from '../../data/bandDatabase'
+
+export const registerBandEndpoint = async (req: Request, res: Response) => {
+    
+}

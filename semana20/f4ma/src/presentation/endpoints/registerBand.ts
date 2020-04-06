4c2895f3dd7f7f790db2ad1802f7a47a2c091c83
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-04-06 02:22:16 -0300

    não consegui terminar os endpoints sem autenticação e detalhas do banco de dados

--- semana20/f4ma/src/presentation/endpoints/registerBand.ts
+++ semana20/f4ma/src/presentation/endpoints/registerBand.ts
@@ -1,6 +1,17 @@
 import {Request, Response} from 'express';
-import {BandDatabase} from '../../data/bandDatabase'
+import {BandDatabase} from '../../data/bandDatabase';
+import {GetBandInfoUC} from '../../business/usecase/getBandInfo';
 
 export const registerBandEndpoint = async (req: Request, res: Response) => {
-    
+    try {
+        const bandId = BandDatabase
+
+        const useCase = new GetBandInfoUC()
+
+        // const input = {
+        //     bandId
+        // }
+
+        //const bandInfo = await useCase.execute(input)
+    }
 }
